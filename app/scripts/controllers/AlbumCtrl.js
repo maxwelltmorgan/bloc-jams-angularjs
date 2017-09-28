9b653d2229c8477995930b99d22f7d7e7148d26b
Maxwell Morgan <maxwelltmorgan@gmail.com>
2017-09-27 22:55:09 -0600

    checkpoint 7

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,11 +1,10 @@
 (function() {
-     function AlbumCtrl(Fixtures) {
-        this.albumData = angular.copy(albumPicasso);
+     function AlbumCtrl(Fixtures, SongPlayer) {
         this.albumData = Fixtures.getAlbum();
+        this.songPlayer = SongPlayer;
      }
 
      angular
          .module('blocJams')
-         .controller('AlbumCtrl', AlbumCtrl);
-         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+         .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
  })();
