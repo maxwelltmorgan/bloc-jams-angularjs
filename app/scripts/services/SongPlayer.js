8d284e6c88094a82d76b32a964f52ea482e2d327
Maxwell Morgan <maxwelltmorgan@gmail.com>
2017-09-27 23:07:09 -0600

    assignment 7

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -27,14 +27,24 @@
               });
 
               currentSong = song;
-              };
+          };
+
+          /**
+          * @function playSong
+          * @desc Plays the selected song
+          * @param {Object} song
+          */
+
+          var playSong = function(song) {
+              currentBuzzObject.play();
+              song.playing = true;
+          };
 
           SongPlayer.play = function(song) {
             if (currentSong !== song) {
 
               setSong(song);
-              currentBuzzObject.play();
-              song.playing = true;
+              playSong(song);
 
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
